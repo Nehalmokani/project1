be0c745ea9de9a171accf604ae0322309cbaf27c
Nehalmokani <96520954+Nehalmokani@users.noreply.github.com>
2022-01-10 08:12:02 +0100

    Update and rename admin.py to Arithmetic.py

---
+++ python/Arithmetic.py
@@ -0,0 +1,9 @@
+a=int(input("enter first value"))
+b=int(input("enter second value"))
+c=int(input("enter 1 for sum, 2 for subtraction"))
+if c==1:
+    print("sum is ",a+b)
+elif c==2:
+    print("sub is",a-b)  
+else:
+    print("wrong choice")
